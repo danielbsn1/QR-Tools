35bba1cf0ad27fddf380f14813156d36344c7031
danielbsn1 <jb375835@icloud.com>
2026-04-07 16:54:41 -0300

    Refactor QR code reading function

--- controle-ferramentas/src/qr_reader.py
+++ controle-ferramentas/src/qr_reader.py
@@ -2,17 +2,17 @@ import cv2
 from pyzbar.pyzbar import decode
 
 def ler_qr_code():
-    cap = cv2.VideoCapture(0)  # Abre a câmera
+    cap = cv2.VideoCapture(0)  
 
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        # Decodifica os QR codes encontrados na imagem
+ 
         decoded_objects = decode(frame)
         for obj in decoded_objects:
-            # Extrai o texto do QR code
+          
             qr_data = obj.data.decode('utf-8')
             cap.release()
             cv2.destroyAllWindows()
@@ -20,7 +20,7 @@ def ler_qr_code():
 
         cv2.imshow("Ler QR Code", frame)
 
-        # Pressione 'q' para sair do loop
+     
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
